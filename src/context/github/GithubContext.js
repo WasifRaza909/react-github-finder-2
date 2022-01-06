e488b773e053a97272a6479d637788136d9c2e7d
wasifraza909 <wasifraza909@gmail.com>
2022-01-06 14:39:02 +0500

    Added functionality to search and clear users

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -14,23 +14,51 @@ export const GithubProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  // Get initial users (testing purposes)
-  const fetchUsers = async () => {
+  // Get initial users (It was for testing purposes)
+  // const fetchUsers = async () => {
+  //   setLoading();
+
+  //   const response = await fetch(`${GITHUB_URL}/users`, {
+  //     headers: {
+  //       Authorization: `token ${GITHUB_TOKEN}`,
+  //     },
+  //   });
+
+  //   const data = await response.json();
+
+  //   dispatch({
+  //     type: 'GET_USERS',
+  //     payload: data,
+  //   });
+  // };
+
+  // Get search results
+  const searchUsers = async (text) => {
     setLoading();
 
-    const response = await fetch(`${GITHUB_URL}/users`, {
+    const params = new URLSearchParams({
+      q: text,
+    });
+
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
       },
     });
 
-    const data = await response.json();
+    const { items } = await response.json();
 
     dispatch({
       type: 'GET_USERS',
-      payload: data,
+      payload: items,
     });
   };
+
+  // Clear users from state
+  const clearUsers = () =>
+    dispatch({
+      type: 'CLEAR_USERS',
+    });
 
   // Set loading
   const setLoading = () =>
@@ -43,7 +71,8 @@ export const GithubProvider = ({ children }) => {
       value={{
         users: state.users,
         loading: state.loading,
-        fetchUsers,
+        searchUsers,
+        clearUsers,
         setLoading,
       }}
     >
